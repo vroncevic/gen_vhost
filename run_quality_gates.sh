34810d3094fe9786713961a6f620f1291ec254c0
Vladimir Roncevic <elektron.ronca@gmail.com>
2026-08-20 07:49:42 +0200

    [gen_vhost] Refactor based on SOLID principles, updated unit tests, docs, added support for content generators

--- run_quality_gates.sh
+++ run_quality_gates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # @brief   gen_vhost
-# @version v1.1.7
+# @version 1.1.8
 # @date    Sat Aug 08 07:35:10 2026
 # @company None, free software to use 2026
 # @author  Vladimir Roncevic <elektron.ronca@gmail.com>
